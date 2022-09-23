b5bbe8bc5ba6774a921f57e732bdd309f9955da4
Caroline Cerussi <caroceru@gmail.com>
2022-09-23 16:24:49 -0700

    research react styled components to try and implement

--- unionize/src/Header.js
+++ unionize/src/Header.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import styled from 'styled-components';
 
-function Header(){
 
+function Header(){
 
 
   const HeadingStyle = styled.h1`
@@ -11,18 +11,23 @@ function Header(){
     background-color: transparent;
     font-family:  ;
     font-size: 40pt;
+    background-image: {me
+}
     `;
 
 
 
   return (
  <HeadingStyle>
-       <Link to="/"> <h1>Unionize</h1></Link>
+       {/* <Link to="/"> <h1>Unionize</h1></Link> */}
+       <h1>Unionize</h1>
       <ul>
       <h />
-         <Link to="/Login">Login</Link>
-         <span><p>Need to Join? <sp />
-         <Link to="/sign-in">Sign Up Here!</Link></p></span>
+      <p>Login</p>
+      <p>Sign Up</p>
+         {/* <Link to="/Login">Login</Link> */}
+         {/* <span><p>Need to Join? <sp /> */}
+         {/* <Link to="/sign-in">Sign Up Here!</Link></p></span> */}
   
          </ul>
 </HeadingStyle>
